8f2136642960e5a9752b6c0c5db924fdfb4c073e
DEVESH-001 <dd4168@srmist.edu.in>
2025-12-12 13:26:36 +0530

    setup-sanity + shadcn

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-## ai-e-commerce #comming soon
+# ai-e-commerce #comming soon
+
+ `Tech Stack`
+
+1. `Nextjs-16`
+2. `React-19`
+3. `Sanity-CMS`
+4. `Vercel-AI`
+5. `Zustand`
+6. `Tailwind-CSS`
+7. `Clerk`
---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      {/* features */}
+      {/* page banner */}
+      {/* category */}
+      {/* products section */}
+    </div>
+  );
+};
+
+export default Home;
